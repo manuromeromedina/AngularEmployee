b675ca9652991f025461265dc052141fb25f646b
manuromeromedina <2105277@ucc.edu.ar>
2024-10-16 10:27:10 -0300

    Push

--- EmployeeCrudAngular/src/test.ts
+++ EmployeeCrudAngular/src/test.ts
@@ -1,25 +1,17 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+// Este archivo es requerido por karma.conf.js y carga recursivamente todos los archivos .spec y del marco.
+(window as any)._env = (window as any)._env || {};
+(window as any).__env['apiUrl'] = window['env'] && window['env']['apiUrl'] || 'mrm-crud-api-qa.eastus.azurecontainer.io/api/employee'; // Valor predeterminado para las pruebas
 
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
+// Primero, inicializa el entorno de pruebas de Angular.
+getTestBed().initTestingModule({
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
-// Then we find all the tests.
+  platformBrowserDynamicTesting(),
+});
+
+// Luego, encontraremos todas las pruebas.
 const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
+context.keys().forEach(context);
